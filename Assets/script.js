f47e73f45be69059fecbf98bdaa58be8c83ed481
Michael Ponte <michaelponte02@hotmail.com>
2022-11-13 01:34:23 -0500

    Commit 3: working memory and finished product

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,6 @@
-var currentDay = moment("2022111216", "YYYYMMDDH").format("MMMM Do YYYY, h:mm:ss a");
-var todaysHour = moment("2022111216", "YYYYMMDDH").format() //YYYY-MM-DDThh:mm:ss-hh:mm
+//moment clocks
+var currentDay = moment().format("MMMM Do YYYY, h:mm:ss a");
+var todaysHour = moment().format() //YYYY-MM-DDThh:mm:ss-hh:mm
 $("#currentDay").text(`Today is: ${currentDay}`);
 
 var input9 = $("#9am");
@@ -14,11 +15,9 @@ var input17 = $("#5pm");
 
 var inputArray = [input9, input10, input11, input12, input13, input14, input15, input16, input17]
 
-console.log(inputArray);
-
-setInterval(function timeUpdate() { //1 second ticks, function runs every tick
-   // currentDay = moment().format("MMMM Do YYYY, h:mm:ss a");
-   // $("#currentDay").text(`Today is: ${currentDay}`);
+setInterval(function timeUpdate() { //200ms ticks, runs 1/tick for quick updates
+   currentDay = moment().format("MMMM Do YYYY, h:mm:ss a");
+   $("#currentDay").text(`Today is: ${currentDay}`);
 
     var justHour = todaysHour.slice(11,13).trim(); //justHour = current hour in 24hr
 
@@ -38,4 +37,48 @@ setInterval(function timeUpdate() { //1 second ticks, function runs every tick
         }
     }
 
-}, 1000);
+}, 200);
+
+function renderSaved () { //renders teh text from memory into each position
+  $(input9).val(localStorage.getItem("9am"));
+  $(input10).val(localStorage.getItem("10am"));
+  $(input11).val(localStorage.getItem("11am"));
+  $(input12).val(localStorage.getItem("12pm"));
+  $(input13).val(localStorage.getItem("1pm"));
+  $(input14).val(localStorage.getItem("2pm"));
+  $(input15).val(localStorage.getItem("3pm"));
+  $(input16).val(localStorage.getItem("4pm"));
+  $(input17).val(localStorage.getItem("5pm"));
+}
+
+
+
+$(".saveBtn").click(function(event) { //Checks for a click
+
+    if (element.matches(".saveBtn") === true) { //Checks if click was on save button
+        var text = $(element).parent().children(".form-control").val() //sets text to value in textbox currently
+        var field = $(element).parent().children(".form-control") //sets field to text box of pressed save button
+        
+        if ($(field).attr("id") == "9am") { //saves text to memory for appropriate text box
+            localStorage.setItem("9am", text);
+        } else if ($(field).attr("id") == "10am") {
+            localStorage.setItem("10am", text);
+        } else if ($(field).attr("id") == "11am") {
+            localStorage.setItem("11am", text);
+        } else if ($(field).attr("id") == "12pm") {
+            localStorage.setItem("12pm", text);
+        } else if ($(field).attr("id") == "1pm") {
+            localStorage.setItem("1pm", text);
+        } else if ($(field).attr("id") == "2pm") {
+            localStorage.setItem("2pm", text);
+        } else if ($(field).attr("id") == "3pm") {
+            localStorage.setItem("3pm", text);
+        } else if ($(field).attr("id") == "4pm") {
+            localStorage.setItem("4pm", text);
+        } else if ($(field).attr("id") == "5pm") {
+            localStorage.setItem("5pm", text);
+        }
+    }
+});
+
+renderSaved(); //renders text from memory at start of application
